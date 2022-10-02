fcd5354893ddeb2a63965ff5fe3adc4371bed11d
Damon Lam <damon.lam.au@gmail.com>
2022-10-02 12:28:20 +1100

    added extra check for collisions

--- arcade_start.py
+++ arcade_start.py
@@ -118,22 +118,45 @@ class MyGame(arcade.Window):
         pline_collisions = (arcade.check_for_collision_with_list(self.perfect_line, self.scene.get_sprite_list(constants.BEAT_LAYER)))
         beats = self.scene.get_sprite_list(constants.BEAT_LAYER)
 
+
+
+
+        # NOTE up and down should be mechanic keys -- grab and combine colours
+        if key == arcade.key.UP or key == arcade.key.W:
+            if (beats.__len__() > 0):
+                hit_beat = beat_in_lane(self.player.lane, beats)
+                if pline_collisions.__contains__(hit_beat):
+                    print("HIT")
+                    beats.remove(hit_beat)
+                    hit_score = hit_beat.hit()
+                    if (hit_score > 0):
+                        # TODO increase bucket and record hit score to calculate overall score out of five at end of round
+                        pass
+
+        elif key == arcade.key.DOWN or key == arcade.key.S:
+            if (beats.__len__() > 0):
+                hit_beat = beat_in_lane(self.player.lane, beats)
+                if hit_beat is not None and pline_collisions.__contains__(hit_beat):
+                    print("HIT")
+                    beats.remove(hit_beat)
+                    hit_score = hit_beat.hit()
+                    if (hit_score > 0):
+                        # TODO increase bucket and record hit score to calculate overall score out of five at end of round
+                        pass
+
         # NOTE up and down should be mechanic keys -- grab and combine colours
         if key == arcade.key.UP or key == arcade.key.W:
             if (beats.__len__() > 0 and pline_collisions.__len__() > 0):
-
                 hit_beat = beat_in_lane(self, self.player.lane, beats)
-                if hit_beat is not None:
+                if hit_beat is not None and pline_collisions.__contains__(hit_beat):
                     print("Perfect!")
-                    for beat in pline_collisions:
-                        if (type(beat) == Beat):
-                            #WARNING WIP
-                            pass
 
                     hit_score = hit_beat.hit()
                     beats.remove(hit_beat)
-                    # self.scene.get_sprite_list(constants.BEAT_LAYER).remove(pline_collisions[0])
-                    # THIS ONE ^^^^
+                    
+                    if (hit_score > 0):
+                        # TODO increase bucket and record hit score to calculate overall score out of five at end of round
+                        pass
 
 
         elif key == arcade.key.DOWN or key == arcade.key.S:
